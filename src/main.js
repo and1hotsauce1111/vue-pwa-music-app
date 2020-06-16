15a5dda1a8ff860fabbfae2780c210905ab2b8a8
and1hotsauce1111 <and1hotsauce1111@gmail.com>
2020-06-16 21:10:31 +0800

    Project init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './common/stylus/index.styl'
+import fastclick from 'fastclick'
 
 Vue.config.productionTip = false
+
+fastclick.attach(document.body)
 
 new Vue({
   router,
