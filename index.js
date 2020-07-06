7483f60b9819a9e1139f5a80096ac202406f21d0
and1hotsauce1111 <and1hotsauce1111@gmail.com>
2020-07-06 11:35:26 +0800

    fixed index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const fs = require('fs')
-const path = require('path')
-const express = require('express')
-const app = express()
+// const fs = require('fs')
+// const path = require('path')
+// const express = require('express')
+// const app = express()
 
-app.use(express.static(path.resolve(__dirname, './dist')))
-app.get('*', function(req, res) {
-  const html = fs.readFileSync(
-    path.resolve(__dirname, './dist/index.html'),
-    'utf-8'
-  )
-  res.send(html)
-})
+// app.use(express.static(path.resolve(__dirname, './dist')))
+// app.get('*', function(req, res) {
+//   const html = fs.readFileSync(
+//     path.resolve(__dirname, './dist/index.html'),
+//     'utf-8'
+//   )
+//   res.send(html)
+// })
+
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
+
+var port = process.env.PORT || 8000
+app.listen(port)
