85e8526a9326e0ebeacc7f5ca89ca1de785de6ea
and1hotsauce1111 <and1hotsauce1111@gmail.com>
2020-07-06 12:16:31 +0800

    add proxy middleware

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-// const fs = require('fs')
-// const path = require('path')
-// const express = require('express')
-// const app = express()
+const express = require('express')
+const path = require('path')
+const serveStatic = require('serve-static')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
-// app.use(express.static(path.resolve(__dirname, './dist')))
-// app.get('*', function(req, res) {
-//   const html = fs.readFileSync(
-//     path.resolve(__dirname, './dist/index.html'),
-//     'utf-8'
-//   )
-//   res.send(html)
-// })
+const app = express()
+// 解決上線後跨域的問題
+const options = {
+  target: 'http://www.kuwo.cn/',
+  changeOrigin: true,
+  ws: true,
+  secure: false,
+  headers: {
+    referer: 'http://www.kuwo.cn/playlists',
+    cookie:
+      'gid=7b2e897d-3dcc-47c3-9386-721a9d000376; Hm_lvt_cdb524f42f0ce19b169a8071123a4797=1593584680; _ga=GA1.2.990725368.1593584680; _gid=GA1.2.1961500553.1593584680; Hm_lpvt_cdb524f42f0ce19b169a8071123a4797=1593593119; kw_token=X7VRWRRJBAB; _gat=1',
+    csrf: 'X7VRWRRJBAB'
+  }
+}
 
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const proxy = createProxyMiddleware(options)
 
-var app = express()
+app.use('/api', proxy)
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
-var port = process.env.PORT || 8000
+const port = process.env.PORT || 8000
 app.listen(port)
